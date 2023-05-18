0e6468ab60517d035ca6b8f2d863eaebe12fdb6a
clown327 <61490369+clown327@users.noreply.github.com>
2023-05-18 22:57:45 +0900

    Update managing.js

--- src/navigation/screens/managing.js
+++ src/navigation/screens/managing.js
@@ -1,12 +1,11 @@
 //민원을 관리할 수 있는 화면 입니다 stack
 import {Text, View} from 'react-native'
 
-export default managing = () => {
-    
+export default function managing() {
     return(
         <View>
             <Text>
-                신고 접수하는 페이지
+                접수된 신고 확인하고 처리하는 페이지 입니다
             </Text>
         </View>
     )
