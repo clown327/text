339468ca47532345c835a5a99f134b8373d9141b
clown327 <61490369+clown327@users.noreply.github.com>
2023-05-18 22:59:53 +0900

    Create complete.js

---
+++ src/navigation/screens/complete.js
@@ -0,0 +1,17 @@
+//ex: 민원을 접수 했습니다. + 버튼 누르면 메인화면으로 돌아가기
+import React from "react";
+import { StyleSheet, Text, View,TouchableOpacity, ScrollView } from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets,} from 'react-native-safe-area-context';
+
+function 
+
+export default function complete()
+{
+  return (
+
+  );
+}
+
+const style = StyleSheet.create({
+
+});
