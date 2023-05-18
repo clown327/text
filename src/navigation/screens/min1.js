e39a4546ab6c6430eb368a1570628311a64fb281
clown327 <61490369+clown327@users.noreply.github.com>
2023-05-18 22:58:45 +0900

    Update min1.js

--- src/navigation/screens/min1.js
+++ src/navigation/screens/min1.js
@@ -1,12 +1,12 @@
 //민원을 누르면 나오는 페이지 stack
 import {Text, View} from 'react-native'
 
-export default managing = () => {
+export default fuction managing(){
     
     return(
         <View>
             <Text>
-                신고 접수하는 페이지
+                들어온 민원을 확인하고 접수하거나 처리하는 페이지 
             </Text>
         </View>
     )
