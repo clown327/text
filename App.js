c561755bb9f9607e072618f49a1a509396379501
clown327 <61490369+clown327@users.noreply.github.com>
2023-05-18 22:47:27 +0900

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StatusBar } from "expo-status-bar";
 import  {StyleSheet, Text, View} from 'react-native';
 
 
-export default function App()
+export default function App() //export default 지우고 실행시켜보기
 {
   return (
     <View style={{flex:1}}>
